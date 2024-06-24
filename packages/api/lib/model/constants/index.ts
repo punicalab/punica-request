81eac4adea9f50216f076bc7bd632806f3c8d9f4
Uryan Sezgin TEKE <sezgin.teke@punicalab.com>
2024-06-24 23:00:16 +0300

    fix: rename files

--- packages/api/lib/model/constants/index.ts
+++ packages/api/lib/model/constants/index.ts
@@ -1,3 +1,3 @@
-export * from './ContentType';
-export * from './RequestMethods';
-export * from './StatusCode';
+export * from './_ContentType';
+export * from './_RequestMethods';
+export * from './_StatusCode';
